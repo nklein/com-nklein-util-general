28c918c8346a0b7ce63d116250284a10335ed4cc
Patrick Stein <pat@nklein.com>
2008-04-28 21:49:09 -0500

    Added bit for making Gaussian-distributed random numbers.

--- package.lisp
+++ package.lisp
@@ -1,9 +1,14 @@
 (defpackage :com.nklein.util.general
     (:use :cl)
     (:export
+	;; from array-things
+	    :make-compatible-array
+	;; from listy-things
+	    :proper-list-p
+	;; from random
+	    :random-gaussian
 	;; from with-argtypes
 	    :with-argtypes
-	    :proper-list-p
 	    :defun-at :flet-at :labels-at :lambda-at
 	    :let-at :let-at*
 	    :do-at :do-at*
